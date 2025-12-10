39b593cf9438b2fb2d79f3e46b674d9e260ea28a
Ilya <Caesar2937@gmail.com>
2025-12-10 18:44:14 +0700

    Update page.tsx

--- app/admin/events/page.tsx
+++ app/admin/events/page.tsx
@@ -1,8 +1,6 @@
 export const dynamic = 'force-dynamic'
 import { AdminEventsClient } from '@/components/admin/admin-events-client'
 
-export const dynamic = 'force-dynamic'
-
 export default function AdminEventsPage() {
   return (
     <div className="min-h-screen ">
